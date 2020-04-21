1eca135914b7012db193b1a20fe6ce0de1abe987
Moki Daniel <mokidnl@gmail.com>
2020-04-21 23:09:10 +0300

    changed previous stateless Javascript funtion to an arrow func but with the same function

--- reactstudies/src/components/Lessonone.js
+++ reactstudies/src/components/Lessonone.js
@@ -1,7 +1,11 @@
 import React from "react"
 
-function Lessonone() {
-    return <h1>This is my first lesson</h1>
-}
+//function Lessonone() {
+    //return <h1>This is my first lesson</h1>
+//}
+
+//function done using ES6
+
+const Lessonone = () => <h1>This is my first lesson </h1>
 
 export default Lessonone
