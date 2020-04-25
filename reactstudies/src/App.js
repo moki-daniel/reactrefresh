18dbf4508df1731bf6ee99f835ef3287b3f75493
Moki Daniel <mokidnl@gmail.com>
2020-04-26 00:10:40 +0300

    The state sample code

--- reactstudies/src/App.js
+++ reactstudies/src/App.js
@@ -3,20 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Lessonone from './components/Lessonone'
 import Classcomponent from './components/Classcomponent'
+import Message from './components/Message'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Lessonone name="react" heroName="king of dynamic websites">
+        <Message />
+       {/*<Lessonone name="react" heroName="king of dynamic websites">
        <button>Click to view</button>
-       </Lessonone>
-       <Lessonone name="node" heroName="The maker of APIS">
+       </Lessonone>*/}
+      { /*<Lessonone name="node" heroName="The maker of APIS">
        <button>Click to view</button>
-       </Lessonone>
-       <Lessonone name="sql" heroName="The most used DB">
+       </Lessonone>*/}
+       {/*<Lessonone name="sql" heroName="The most used DB">
        <button>Click to view</button>
-       </Lessonone>
+       </Lessonone> */}
 
        {/*<Classcomponent />*/}
        {/*<Classcomponent />*/}
