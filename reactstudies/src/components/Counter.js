5db8218165bd2c437fb8b8ae82842fea569e248b
Moki Daniel <mokidnl@gmail.com>
2020-04-26 11:54:21 +0300

    added the setState increment count by five time

--- reactstudies/src/components/Counter.js
+++ reactstudies/src/components/Counter.js
@@ -7,20 +7,31 @@ class Counter extends Component {
            count: 0
         }
     }
-    increment(){
-       this.setState({
-           count: this.state.count + 1
-       }, () =>{
-           console.log('callback value', this.state.count)
-       } )
+    increment() {
+      // this.setState({
+           //count: this.state.count + 1
+       //}, () =>{
+           //console.log('callback value', this.state.count)
+      // } )
+      this.setState((prevState, props) =>({
+          count: prevState.count + 1
+      }))
        console.log(this.state.count)
     }
+    incrementFive() {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+
+     }
     
   render() {
       return (
       <div>
           <div> Count - {this.state.count} </div>
-          <button onClick = {() => this.increment()}> Increment </button>
+          <button onClick = {() =>this.incrementFive()}> Increment </button>
         </div>
         )
     }
