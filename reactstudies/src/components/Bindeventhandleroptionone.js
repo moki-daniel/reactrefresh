08a18486dcc597d64e3903738778baf202db2190
Moki Daniel <mokidnl@gmail.com>
2020-04-28 21:03:40 +0300

    added a binding event handler in the constructor

--- reactstudies/src/components/Bindeventhandleroptionone.js
+++ reactstudies/src/components/Bindeventhandleroptionone.js
@@ -7,6 +7,7 @@ class Bindeventhandleroptionone extends Component {
       this.state = {
          message: 'Hello, click to bind this event'
       }
+      this.clickHandler = this.clickHandler.bind(this)
     }
 
     clickHandler() {
@@ -15,12 +16,14 @@ class Bindeventhandleroptionone extends Component {
         })
         console.log(this)
     }
-    
+
   render() {
     return (
       <div>
           <div>{this.state.message}</div>
-          <button onClick={this.clickHandler.bind(this)}>Binded Click</button>
+    {/*<button onClick={this.clickHandler.bind(this)}>Binded Click</button>*/}
+    {/*<button onClick={() => this.clickHandler()}>Binded Click</button>*/}
+    <button onClick={this.clickHandler}>Binded Click</button>
       </div>
     )
   }
